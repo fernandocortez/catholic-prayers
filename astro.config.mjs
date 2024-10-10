c5a8dc2d2a715ffa26cf7263a6a53b05c260ab43
fernandocortez <development+github@fernandocortez.dev>
2024-10-10 09:46:20 -0400

    fix: Site metadata now using Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://catholic-prayers-oraciones-catolicas.netlify.app',
+  site: 'https://catholic-prayers.pages.dev/',
   integrations: [mdx(), sitemap()],
 });
