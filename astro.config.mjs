89e2634b98ef225a0774621a517c11b2d09ae5b5
Fernando Cortez <development+github@fernandocortez.dev>
2025-11-26 17:21:22 +0000

    feat: VS Code dev containers

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true,
+  },
   site: 'https://catholic-prayers.pages.dev/',
   integrations: [mdx(), sitemap()],
 });
